4458d8105a162e131b136b1b6487568e56f1ff6f
Strelok <>
2026-08-14 22:22:25 +0200

    Replace GTM with Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import SiteShell from "@/components/SiteShell";
 import "./globals.css";
 
-const GTM_ID = "GTM-PM4HMG2G";
+const GOOGLE_ANALYTICS_ID = "G-133RNTTSW2";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -50,24 +50,18 @@ export default function RootLayout({
   return (
     <html lang="de" className={`${inter.variable} ${archivo.variable}`}>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GOOGLE_ANALYTICS_ID}');`}
         </Script>
       </head>
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-            title="Google Tag Manager"
-          />
-        </noscript>
         <SiteShell>{children}</SiteShell>
       </body>
     </html>
